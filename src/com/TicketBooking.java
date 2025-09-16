5a9606a1c048bc1d1c7397db043081d1acdbbbb6
keshav <k21keshav05@gmail.com>
2025-09-16 18:13:00 +0530

    TicketBooking.java - bookingTicket() Logic

--- src/com/TicketBooking.java
+++ src/com/TicketBooking.java
@@ -13,12 +13,34 @@ public class TicketBooking {
 
     private int ticketCounter = 1;
 
-    public void bookTicket(String name, int age, String gender, String berthPreference){
+    private void bookTicket(String name, int age, String gender, String berthPreference){
         String ticketId = "T" + ticketCounter++;
         Passenger passenger;
 
         if(!availableBerth.isEmpty()){
             String allotedBerth = allotedBerth(gender, age, berthPreference);
+            passenger = new Passenger(name, age, gender, berthPreference, allotedBerth, ticketId);
+
+            confirmedPassengers.add(passenger);     // Add the confirmed passenger if they get the alloted berth
+
+            availableBerth.remove(allotedBerth);    // Remove the alloted berth after giving it to a passenger as it has been occupied
+
+            System.out.println("Ticket Confirmed : "+passenger);    // Prints the details of the Passenger
+
+        }else if(racQueue.size() < 1){  // if the availableBerth is full
+            passenger = new Passenger(name, age, gender, berthPreference, "RAC", ticketId);
+
+            racQueue.offer(passenger);                           // Adds the passenger in tha tail of the RAC queue
+            System.out.println("Ticket in RAC : "+passenger);    // Prints the details of the Passenger
+
+        }else if(waitingList.size() < 1){  // if the racQueue is full
+            passenger = new Passenger(name, age, gender, berthPreference, "RAC", ticketId);
+
+            waitingList.offer(passenger);                                 // Adds the passenger in tha tail of the waitingList queue
+            System.out.println("Ticket in Waiting List : "+passenger);    // Prints the details of the Passenger
+
+        }else{
+            System.out.println("NO TICKETS AVAILABLE !!");  // this happens otherwise....
         }
 
 
@@ -34,4 +56,7 @@ public class TicketBooking {
 
         return availableBerth.get(0);
     }
+
+    
+
 }
