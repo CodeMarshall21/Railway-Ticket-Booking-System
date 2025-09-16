62e8e1f61bde1f3a8c4cf2a1a8e95f09455c59ae
keshav <k21keshav05@gmail.com>
2025-09-16 15:55:42 +0530

    Passenger.java Class

--- src/com/Passenger.java
+++ src/com/Passenger.java
@@ -1,4 +1,24 @@
 package com;
 
 public class Passenger {
+    String name;
+    int age;
+    String gender;
+    String berthPreference;
+    String allotedBerth;
+    String ticketId;
+
+    public Passenger(String name,int age,String gender,String berthPreference,String allotedBerth,String ticketId){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.berthPreference = berthPreference;
+        this.allotedBerth = allotedBerth;
+        this.ticketId = ticketId;
+    }
+
+    @Override
+    public String toString(){
+        return "TicketId: " + ticketId + ", Name: " + name + "Age: " + age + ", Gender: " + gender + ", Alloted Berth: "+allotedBerth;
+    }
 }
